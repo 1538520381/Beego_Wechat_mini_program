1d9c6c6e24c1fdb97f19e8f101a5b9b1635ec4c8
Persolute <1538520381@qq.com>
2024-12-11 14:34:09 +0800

    登录过的用户直接进入

--- pages/home/home.js
+++ pages/home/home.js
@@ -24,6 +24,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    getUserByToken().then((res) => {
+      if (res.data.code === 200) {
+        wx.switchTab({
+          url: '/pages/workbench/workbench'
+        })
+      }
+    })
     this.createChatClock1();
   },
 
